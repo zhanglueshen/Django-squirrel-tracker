1aa710e538e76d0f47e8dfe7606576b6ea5a99cb
Zhanglue Shen <zs2440@szl-1.c.sixth-window-254004.internal>
2019-12-05 22:37:36 +0000

    szl import success

--- adopt/urls.py
+++ adopt/urls.py
@@ -3,7 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-        path('list/',views.all_pets),
-        path('<int:pet_id>',views.pet_details),
-        path('<int:pet_id>/edit/',view.edit_pet),
+    
 ]
