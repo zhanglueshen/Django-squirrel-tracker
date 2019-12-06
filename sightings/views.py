2e4b354535f848ff427f6dc59f654e53a6f9119d
zhanglueshen <zs2440@columbia.edu>
2019-12-06 21:09:50 +0000

    szl creat stats

--- sightings/views.py
+++ sightings/views.py
@@ -38,4 +38,14 @@ def add(request):
     else:
         form = SquirrelForm()
     return render(request, 'sightings/add.html', {'form':form})
+
+def stats(request):
+    sq_data=Squirrel.objects.all()
+    a=len(sq_data)
+    b=sq_data.aggregate(min_latitude=Min('latitude'),max_latitude=Max('latitude'),average_latitude=Avg('latitude'))
+    c=sq_data.aggregate(min_longitude=Min('longitude'),max_longitude=Max('longitude'),average_longitude=Avg('longitude'))
+    d=list(sq_data.values_list('shift').annotate(Count('shift')))
+    e=list(sq_data.values_list('age').annotate(Count('age')))
+    f=list(sq_data.values_list('primary_fur_color').annotate(Count('primary_fur_color')))
+    return render(request, 'sightings/stats.html', {"a":a,"b":b,"c":c,"d":d,"e":e,"f":f})
 # Create your views here.                                                           
