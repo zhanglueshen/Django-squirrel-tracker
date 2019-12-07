e12ee5be3d91a73ff336be5e2f5f64ca107c6781
zhanglueshen <zs2440@columbia.edu>
2019-12-07 16:17:42 +0000

    szl change stats

--- sightings/views.py
+++ sightings/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
 from django.template import loader
+from django.views.generic.edit import CreateView
 from django.db.models import Count, Q, Avg, Max, Min, Count
 
 from .models import Squirrel
@@ -48,14 +49,11 @@ def add(request):
     return render(request, 'sightings/add.html', context)
 
 def stats(request):
-    sq_data=Squirrel.objects.all()
-    a=len(sq_data)
-    b=sq_data.aggregate(min_latitude=Min('latitude'),max_latitude=Max('latitude'),average_latitude=Avg('latitude'))
-    c=sq_data.aggregate(min_longitude=Min('longitude'),max_longitude=Max('longitude'),average_longitude=Avg('longitude'))
-    d=list(sq_data.values_list('shift').annotate(Count('shift')))
-    e=list(sq_data.values_list('age').annotate(Count('age')))
-    f=list(sq_data.values_list('color').annotate(Count('color')))
-    return render(request, 'sightings/stats.html', {"a":a,"b":b,"c":c,"d":d,"e":e,"f":f})
+	squirrels = Squirrel.objects.all()
+	lattitude = squirrels.aggregate(min_latitude=Min('latitude'),max_latitude=Max('latitude'))
+	longitude = squirrels.aggregate(min_latitude=Min('latitude'),max_latitude=Max('latitude'))
+	context = {'squirrels': squirrels}
+	return render(request, 'sighting/stats.html', context)
 
 def map(request):
     sightings = Squirrel.objects.all()[:100]
